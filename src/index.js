9764edb81cfb0c168d5358985249d5db5d4ade8f
Edvard_Munch <nikolay.pavlyuchkov@gmail.com>
2020-12-20 02:40:02 +0100

    Normal guy

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import 'phaser';
+
+var config = {
+	type: Phaser.AUTO,
+	parent: 'phaser-example',
+	width: 800,
+	height: 600,
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
+};
+
+var game = new Phaser.Game(config);
+
+function preload()
+{
+	this.load.multiatlas('cityscene', 'assets/cityscene.json', 'assets');
+}
+
+var capguy;
+
+function create () 
+{
+	var background = this.add.sprite(0, 0, 'cityscene', 'background.png');
+
+	capguy = this.add.sprite(0, 400, 'cityscene', 'capguy/walk/0001.png');
+	capguy.setScale(0.5, 0.5);
+	
+	var frameNames = this.anims.generateFrameNames('cityscene', {
+		start: 1, end: 8, zeroPad: 4, prefix: 'capguy/walk/', suffix: '.png'
+	});
+
+	this.anims.create({ 
+		key: 'walk', frames: frameNames, frameRate: 10, repeat: -1
+	});
+	capguy.anims.play('walk');
+
+}
+
+
+function update(time, delta)
+{
+	capguy.x += delta/8;
+	if(capguy.x > 800)
+	{
+		capguy.x = -50;
+	}
+}
